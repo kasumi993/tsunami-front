95dc2bc56b4ed06119899492fa10ac6aa9e6353e
Khady GUEYE <khadijag993@gmail.com>
2022-11-02 17:07:07 +0100

    save initial setup

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img src="./assets/circle-top.svg" class="circle-left">
+  <img src="./assets/circle-left.svg" class="circle-right">
+  <div class="flex row">
+    <LeftMenu></LeftMenu>
+    <RightSection></RightSection>
+  </div>
+</template>
+
+<script>
+import LeftMenu from "@/components/LeftMenu.vue";
+import RightSection from "@/components/RightSection.vue";
+
+export default {
+  name: 'App',
+  components: {
+    RightSection,
+    LeftMenu,
+  }
+}
+</script>
+
+<style>
+@import "style/style.scss";
+</style>
